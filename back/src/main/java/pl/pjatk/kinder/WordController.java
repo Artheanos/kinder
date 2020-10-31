3d7d9dff19f9dc04d3ef66cf81a291e28cda7636
Jan <jannos1999@gmail.com>
2020-11-01 00:40:36 +0100

    added CORS header

--- back/src/main/java/pl/pjatk/kinder/WordController.java
+++ back/src/main/java/pl/pjatk/kinder/WordController.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/word")
 public class WordController {
 
