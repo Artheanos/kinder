14c01911641802dad3b8f4dd2d6fa83725556d1b
oskar117 <olek.konieczny@wp.pl>
2020-11-08 11:33:34 +0100

    Adjust database settings

--- back/src/main/java/pl/pjatk/kinder/security/SecurityConfig.java
+++ back/src/main/java/pl/pjatk/kinder/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/word").authenticated()
                 .and()
                 .addFilter(new JwtFilter(authenticationManager()));
--- back/src/main/java/pl/pjatk/kinder/words/WordController.java
+++ back/src/main/java/pl/pjatk/kinder/words/WordController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/word")
 public class WordController {
 
