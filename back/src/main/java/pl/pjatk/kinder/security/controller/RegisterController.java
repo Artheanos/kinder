260e34c120ea4820487937a7183bd26d1b38042f
oskar117 <olek.konieczny@wp.pl>
2020-11-15 19:37:57 +0100

    Add bean validation for requests

--- back/src/main/java/pl/pjatk/kinder/security/controller/RegisterController.java
+++ back/src/main/java/pl/pjatk/kinder/security/controller/RegisterController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import pl.pjatk.kinder.security.model.RegisterRequest;
 import pl.pjatk.kinder.security.model.ResponseMessage;
 import pl.pjatk.kinder.security.model.User;
 import pl.pjatk.kinder.security.repo.UserRepository;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/register")
@@ -25,7 +28,12 @@ public class RegisterController {
     }
 
     @PostMapping
-    public ResponseEntity register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity register(@Valid @RequestBody RegisterRequest registerRequest, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(new ResponseMessage(errors.getAllErrors().get(0).getDefaultMessage()));
+        }
+
         if (userRepository.existsByEmail(registerRequest.getEmail())) {
             return ResponseEntity.badRequest().body(new ResponseMessage("Email already in use!"));
         }
