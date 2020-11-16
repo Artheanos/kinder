07babf733b225289ee83958c036490411030cd37
Jan Pieczul <30315254+Artheanos@users.noreply.github.com>
2020-11-16 14:28:58 +0100

    removed regex matching for logging credentials

--- back/src/main/java/pl/pjatk/kinder/security/model/LoginRequest.java
+++ back/src/main/java/pl/pjatk/kinder/security/model/LoginRequest.java
@@ -4,9 +4,7 @@ import javax.validation.constraints.Pattern;
 
 public class LoginRequest {
 
-    @Pattern(regexp = "^\\S+@\\S+$", message = "Invalid email format")
     String email;
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "Invalid password format")
     String password;
 
     public LoginRequest(String email, String password) {
